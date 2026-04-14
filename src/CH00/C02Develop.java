ef75a2c61a7c72fc41375161d89c837cb9f5cf27
parkhyeonggyu15 <gudrb15@naver.com>
2026-04-14 15:33:40 +0900

    V0.2 Develop(조장 : 홍길동)

--- src/CH00/C02Develop.java
+++ src/CH00/C02Develop.java
@@ -4,7 +4,7 @@ public class C02Develop {
 
 	public static void main(String[] args) {
 
-		//V0.0 Master(조장 : 홍길동)
+		//V0.1 Devloper(조장 : 홍길동)
 		System.out.println("V0.1 Devloper(조장 : 홍길동)");
 
 	}
